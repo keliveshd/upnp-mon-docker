40de7600f7695679578537db95c2badb1b9b5d57
evilek@qq.com <windows1>
2021-01-19 15:51:22 +0800

    commit file

--- Dockerfile/docker-entrypoint.sh
+++ Dockerfile/docker-entrypoint.sh
@@ -4,13 +4,15 @@ if [ ! -f "/upnp-mon/config/upnp-mon.conf" ]; then
 fi
 
 if [ ! -f "/upnp-mon/config/upnp-mon.rules.conf" ]; then
-	cp /upnp-mon/upnp-mon.rules /upnp-mon/config/upnp-mon.rules.conf
+	cp /upnp-mon/upnp-mon.rules.conf /upnp-mon/config/upnp-mon.rules.conf
 fi
 
 if [ ! -f "/upnp-mon/config/cron.conf" ]; then
 #	echo '*  *  *  *  *    bash /upnp-mon/upnp-mon>/upnp-mon/config/connect.log 2>&1' > /upnp-mon/config/cron.conf
 	echo '0 0/5 *  *  *    bash /upnp-mon/upnp-mon>/upnp-mon/config/connect.log 2>&1' > /upnp-mon/config/cron.conf
 fi
+
+
 
 cp /upnp-mon/config/cron.conf /etc/crontabs/root
 touch /upnp-mon/config/connect.log
